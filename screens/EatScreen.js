9a365597d0627f4cbd609f66afd1fa27542273c6
Shayan Shuaib <shayan3742@gmail.com>
2021-10-07 18:47:58 +0500

    Initial commit

---
+++ screens/EatScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const EatScreen = () => {
+  return (
+    <View>
+      <Text>This is Eat screen</Text>
+    </View>
+  );
+};
+
+export default EatScreen;
---
+++ screens/MapScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>This is map screen</Text>
+    </View>
+  );
+};
+
+export default MapScreen;
